dba3740f931b5829a7bc87524b88120f066027d6
TharinduWijewardane <tharinduw@wso2.com>
2024-10-16 14:01:47 +0530

    Fix oomkill sim

--- go/main.go
+++ go/main.go
@@ -4,13 +4,11 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
 	http.HandleFunc("/", handleRoot)
 	http.HandleFunc("/trigger", handleTrigger)
-
 	log.Printf("Server starting ...")
 	if err := http.ListenAndServe(":9090", nil); err != nil {
 		log.Fatal(err)
@@ -27,19 +25,21 @@ func handleTrigger(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-
 	go simulateOOMKill()
-
 	io.WriteString(w, "OOMKill simulation triggered\n")
 }
 
 func simulateOOMKill() {
 	log.Println("Starting OOMKill simulation")
-
-	var mem [][]int
+	var memory [][]byte
+	chunkSize := 500 * 1024 * 1024 // 100MB chunks
 	for {
-		mem = append(mem, make([]int, 1024*1024*100))
-		log.Printf("Allocated memory: %d MB", len(mem))
-		time.Sleep(100 * time.Millisecond)
+		memory = append(memory, make([]byte, chunkSize))
+		allocatedMB := len(memory) * 500
+		log.Printf("Allocated memory: %d MB", allocatedMB)
+		// Fill the allocated memory with non-zero values
+		for i := range memory[len(memory)-1] {
+			memory[len(memory)-1][i] = byte(i % 256)
+		}
 	}
 }
